7c0d369bc2cfd2f198e9a042c251bbb65a39c700
wesleywil <wesleywilsonti@gmail.com>
2023-01-24 14:01:43 -0300

    updated App.tsx by adding two new pages sign_in_and_sign_up and register_cellphone

--- hc_suporte/src/App.tsx
+++ hc_suporte/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+
+// Pages
 import Homepage from "./pages/homepage/homepage";
+import SignInAndSignUp from "./pages/sign_in_and_sign_up/sign_in_and_sign_up";
+import RegisterCellphone from "./pages/register_cellphone/register_cellphone";
 
 function App() {
   return (
@@ -8,6 +12,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/logar" element={<SignInAndSignUp />} />
+          <Route path="/registrar" element={<RegisterCellphone />} />
         </Routes>
       </BrowserRouter>
     </div>
