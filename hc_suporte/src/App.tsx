178cc089f127907193ecf63c94b9ec35b72d2ef4
wesleywil <wesleywilsonti@gmail.com>
2023-01-28 13:07:34 -0300

    updated App.tsx by adding admin services page

--- hc_suporte/src/App.tsx
+++ hc_suporte/src/App.tsx
@@ -8,6 +8,7 @@ import RegisterCellphone from "./pages/register_cellphone/register_cellphone";
 import Problem from "./pages/problem/problem";
 import ServiceOrders from "./pages/services_orders/service_orders";
 import Clients from "./admin/pages/clients/clients";
+import Services from "./admin/pages/services/services";
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
           <Route path="/registrar" element={<RegisterCellphone />} />
           <Route path="/problema" element={<Problem />} />
           <Route path="/ordem_servicos" element={<ServiceOrders />} />
-          <Route path="/admin/clients" element={<Clients />} />
+          <Route path="/admin/clientes" element={<Clients />} />
+          <Route path="/admin/servicos" element={<Services />} />
         </Routes>
       </BrowserRouter>
     </div>
