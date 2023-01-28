cc8c76b0e2413de7619492813077e6063e3b2562
wesleywil <wesleywilsonti@gmail.com>
2023-01-28 12:19:54 -0300

    updated clients page and components by adding redux reducer to switch client_list view

--- hc_suporte/src/admin/components/clients_item/clients_item.component.tsx
+++ hc_suporte/src/admin/components/clients_item/clients_item.component.tsx
@@ -1,16 +1,25 @@
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../redux/store";
+import { switch_client_list_view } from "../../../redux/admin/admin";
+
 type ClientsItemProps = {
   title: string;
   quantity: number;
 };
 
 const ClientsItem = ({ title, quantity }: ClientsItemProps) => {
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <div className="self-center w-full p-2 bg-[#4b9978] text-center text-[#0a191e] text-2xl rounded-xl">
       <h3 className="font-bold">{title}</h3>
       <h1 className="w-10  h-10 mx-auto font-semibold text-white border-2 border-white rounded-full ">
         {quantity}
       </h1>
-      <button className="bg-[#0a191e] text-white active:opacity-80 px-2 py-1 rounded-xl mt-2">
+      <button
+        onClick={() => dispatch(switch_client_list_view())}
+        className="bg-[#0a191e] text-white active:opacity-80 px-2 py-1 rounded-xl mt-2"
+      >
         Ver lista
       </button>
     </div>
--- hc_suporte/src/admin/pages/clients/clients.tsx
+++ hc_suporte/src/admin/pages/clients/clients.tsx
@@ -1,10 +1,17 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../../../redux/store";
+
 import ClientsItem from "../../components/clients_item/clients_item.component";
 import ClientList from "../../components/client_list/client_list.component";
 
 const Clients = () => {
+  const list_view_hidden = useSelector(
+    (state: RootState) => state.admin.client_list_hidden
+  );
   return (
     <div className="min-h-full h-screen p-2">
-      <ClientList />
+      {list_view_hidden ? "" : <ClientList />}
+
       <h1 className="text-4xl text-white text-center font-bold pt-2">
         Clientes
       </h1>
