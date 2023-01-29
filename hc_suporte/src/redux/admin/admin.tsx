4e73eb456f55b67e00ea8af27dad7ab7d3fa10f2
wesleywil <wesleywilsonti@gmail.com>
2023-01-29 14:01:54 -0300

    updated admin reducer by including two new actions and states for allowing the client info and service info to be view

--- hc_suporte/src/redux/admin/admin.tsx
+++ hc_suporte/src/redux/admin/admin.tsx
@@ -2,10 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface AdminState {
   client_list_hidden: boolean;
+  service_info_hidden: boolean;
+  client_info_hidden: boolean;
 }
 
 const initialState: AdminState = {
   client_list_hidden: true,
+  service_info_hidden: true,
+  client_info_hidden: true,
 };
 
 export const adminSlice = createSlice({
@@ -15,9 +19,19 @@ export const adminSlice = createSlice({
     switch_client_list_view: (state) => {
       state.client_list_hidden = !state.client_list_hidden;
     },
+    switch_service_info_view: (state) => {
+      state.service_info_hidden = !state.service_info_hidden;
+    },
+    switch_client_info_view: (state) => {
+      state.client_info_hidden = !state.client_info_hidden;
+    },
   },
 });
 
-export const { switch_client_list_view } = adminSlice.actions;
+export const {
+  switch_client_list_view,
+  switch_service_info_view,
+  switch_client_info_view,
+} = adminSlice.actions;
 
 export default adminSlice.reducer;
