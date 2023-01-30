12fd9a288f7bf4611d2590391c8dd110e8838cfa
wesleywil <wesleywilsonti@gmail.com>
2023-01-30 13:53:17 -0300

    updated signUp now a user can be created and stored in the database using firebase

--- hc_suporte/src/components/signUp/signUp.component.tsx
+++ hc_suporte/src/components/signUp/signUp.component.tsx
@@ -1,10 +1,51 @@
 import FormButton from "../form_button/form_button.component";
+import { useNavigate } from "react-router-dom";
+
+interface CustomElements extends HTMLFormControlsCollection {
+  displayName: HTMLInputElement;
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+  password2: HTMLInputElement;
+}
+
+interface CustomForm extends HTMLFormElement {
+  readonly elements: CustomElements;
+}
+
+import { handleSignUp } from "../../firebase/config";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e: React.FormEvent<CustomForm>) => {
+    e.preventDefault();
+    console.log("Creating new User");
+    const target = e.currentTarget.elements;
+
+    if (target.password.value !== target.password2.value) {
+      alert("Senhas não são iguais!");
+    } else {
+      const data = {
+        displayName: target.displayName.value,
+        email: target.email.value,
+        password: target.password.value,
+      };
+      handleSignUp(data);
+      navigate("/");
+    }
+  };
   return (
     <div className="self-center mt-2 p-4 text-3xl text-[#d9b55d]">
       <h1 className="mb-2 text-center">Novo Registro</h1>
-      <form className="flex flex-col gap-2 text-2xl text-black">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2 text-2xl text-black"
+      >
+        <input
+          type="text"
+          name="displayName"
+          placeholder="Nome"
+          className="rounded-xl px-2"
+        />
         <input
           type="email"
           name="email"
---
+++ hc_suporte/src/firebase/config.ts
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import { getFirestore, collection, doc, getDocs, getDoc, setDoc } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA8f7ZKMJDhr---F3wdvxJQsleHVQuhKRI",
+    authDomain: "hc-suporte.firebaseapp.com",
+    projectId: "hc-suporte",
+    storageBucket: "hc-suporte.appspot.com",
+    messagingSenderId: "430710885800",
+    appId: "1:430710885800:web:a507705e795890ac64e19d",
+    measurementId: "G-RP980QZ6JX"
+  };
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+
+export const handleSignUp = async(data:any) =>{
+    try{
+        const result = await createUserWithEmailAndPassword(auth, data.email, data.password)
+
+        const userRef = doc(db, `users/${result.user.uid}`);
+        await setDoc(userRef, {
+            displayName:data.displayName,
+            email:result.user.email,
+            uid:result.user.uid
+        })
+    }catch(error:any){
+        console.log('ERROR => ', error.message);
+    }
+}
+
+// export const getCurrentUser = ()=>{
+//     return new Promise((resolve, reject)=>{
+//         const unsubscribe = auth.onAuthStateChanged(userAuth =>{
+//             unsubscribe();
+//             resolve(userAuth);
+//         }, reject)
+//     })
+// }
