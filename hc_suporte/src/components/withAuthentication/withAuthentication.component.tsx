2638d887acf0ff6e313be62d388a933a32cba59a
wesleywil <wesleywilsonti@gmail.com>
2023-02-05 16:24:37 -0300

    updated component by adding a loading while the UID is retrieved from the database, was bugging and not giving access to the pages

--- hc_suporte/src/components/withAuthentication/withAuthentication.component.tsx
+++ hc_suporte/src/components/withAuthentication/withAuthentication.component.tsx
@@ -3,13 +3,29 @@ import type { RootState } from "../../redux/store";
 import { Navigate } from "react-router-dom";
 import { useEffect, ComponentType } from "react";
 
+import Loading from "../../admin/components/loading/loading.component";
+
 function WithAuthentication<T>(Component: ComponentType<T>, hocProps?: any) {
   const user_id = useSelector((state: RootState) => state.account.user_id);
+  const admin = useSelector((state: RootState) => state.account.admin);
   useEffect(() => {}, [user_id]);
-  if (user_id !== "") {
-    return <Component {...hocProps} />;
+  if (admin === "idle") {
+    return (
+      <div className="h-screen">
+        <div className="h-full flex flex-col items-center justify-center">
+          <div className="self-center">
+            <Loading />
+          </div>
+          <h1 className="text-white">Carregando...</h1>
+        </div>
+      </div>
+    );
   } else {
-    return <Navigate to="/logar" replace={true} />;
+    if (user_id !== "") {
+      return <Component {...hocProps} />;
+    } else {
+      return <Navigate to="/logar" replace={true} />;
+    }
   }
 }
 
