031df20c0245d4b34cdf929adde2920df5ff3e13
wesleywil <wesleywilsonti@gmail.com>
2023-02-05 16:25:28 -0300

    updated form_problem component by adding the handleSubmit, now we can register a new problem by the client

--- hc_suporte/src/components/form_problem/form_problem.component.tsx
+++ hc_suporte/src/components/form_problem/form_problem.component.tsx
@@ -1,28 +1,76 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../redux/store";
+import { useNavigate } from "react-router-dom";
+import { listCellphonesByUserId } from "../../firebase/cellphone/cellphone_config";
+import { registerProblem } from "../../firebase/problem/problem_config";
+
 import FormButton from "../form_button/form_button.component";
 import SelectDay from "../select_day/select_day.component";
 import SelectMonth from "../select_month/select_month.component";
 
+interface CustomElements extends HTMLFormControlsCollection {
+  cellphone: HTMLInputElement;
+  problem: HTMLInputElement;
+  uid: HTMLInputElement;
+}
+
+interface CustomForm extends HTMLFormElement {
+  readonly elements: CustomElements;
+}
+
 const FormProblem = () => {
+  const navigate = useNavigate();
+  const user_id = useSelector((state: RootState) => state.account.user_id);
+  const [cellphones, setCellphones]: any = useState([]);
+
+  useEffect(() => {
+    listCellphonesByUserId(user_id).then((doc) => {
+      console.log(doc);
+      setCellphones(doc);
+    });
+  }, [user_id]);
+
+  const handleSubmit = (e: React.FormEvent<CustomForm>) => {
+    e.preventDefault();
+    console.log("Creating new problem register");
+    const target = e.currentTarget.elements;
+
+    const data = {
+      cellphone: target.cellphone.value,
+      problem: target.problem.value,
+      uid: user_id,
+    };
+    registerProblem(data);
+    navigate("/");
+  };
   return (
-    <form className="flex flex-col gap-2 text-2xl text-black">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 text-2xl text-black"
+    >
       <label className="text-xl text-[#d9b55d]">Selecione Celular</label>
-      <select className="rounded-xl px-2">
-        <option value="01">Cellphone 01</option>
-        <option value="02">Cellphone 02</option>
+      <select name="cellphone" className="rounded-xl px-2">
+        {cellphones.map((data: any, i: number) => (
+          <option key={i} defaultValue={data.model}>
+            {data.brand} - {data.model}
+          </option>
+        ))}
       </select>
       <label className="text-xl text-[#d9b55d]">Problema</label>
       <textarea
+        name="problem"
         rows={5}
         placeholder="Ex: a tela esta quebrada, gostaria de troca-la"
         className="rounded-xl px-2"
       ></textarea>
-      <label className="text-xl text-[#d9b55d]">Reservar Dia</label>
+      {/* <label className="text-xl text-[#d9b55d]">Reservar Dia</label>
       <div className="flex gap-4 justify-center">
         <SelectMonth />
         <SelectDay />
-      </div>
+      </div> */}
       <div className="flex gap-2 justify-center mt-2">
-        <FormButton name="Order" type="submit" />
+        <FormButton name="Criar" type="submit" />
         <FormButton name="Cancelar" type="button" />
       </div>
     </form>
--- hc_suporte/src/firebase/problem/problem_config.ts
+++ hc_suporte/src/firebase/problem/problem_config.ts
@@ -6,10 +6,9 @@ export const registerProblem = async(data:any)=>{
     const createdAt = new Date();
     try{ 
         const proRef = await addDoc(collection(db, "problems"),{
-            cellphone: data.cellphone,
-            problem_description:data.problem_description,
+            ...data,
             created_at: createdAt,
-            uid:data.uid,
+            
         })
         console.log('Document Writen with ID=> ', proRef.id)
     }catch(error:any){
