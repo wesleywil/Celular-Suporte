795f75c1b20f20037d70b03ae29be12866941752
wesleywil <wesleywilsonti@gmail.com>
2023-02-07 14:05:06 -0300

    added problems Slicer and Reducers

---
+++ hc_suporte/src/redux/problems/problems.tsx
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Problem {
+  cellphone: string;
+  created_at: string;
+  problem: string;
+  status: string;
+  uid: string;
+}
+
+export interface ProblemsState {
+  selected_problems?: Array<Problem>;
+  open_problems?: Array<Problem>;
+  inProgress_problems?: Array<Problem>;
+  closed_problems?: Array<Problem>;
+  denied_problems?: Array<Problem>;
+}
+
+const initialState: ProblemsState = {
+  selected_problems: [],
+  open_problems: [],
+  inProgress_problems: [],
+  closed_problems: [],
+  denied_problems: [],
+};
+
+export const problemsSlice = createSlice({
+  name: "problems",
+  initialState,
+  reducers: {
+    set_selected_problems: (state, { payload }) => {
+      state.selected_problems = payload;
+    },
+    set_open_problems: (state, { payload }) => {
+      state.open_problems = payload;
+    },
+    set_inProgress_problems: (state, { payload }) => {
+      state.inProgress_problems = payload;
+    },
+    set_closed_problems: (state, { payload }) => {
+      state.closed_problems = payload;
+    },
+    set_denied_problems: (state, { payload }) => {
+      state.denied_problems = payload;
+    },
+  },
+});
+
+export const {
+  set_selected_problems,
+  set_open_problems,
+  set_inProgress_problems,
+  set_closed_problems,
+  set_denied_problems,
+} = problemsSlice.actions;
+
+export default problemsSlice.reducer;
--- hc_suporte/src/redux/store.ts
+++ hc_suporte/src/redux/store.ts
@@ -1,14 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware  } from "@reduxjs/toolkit";
 import adminReducer from "./admin/admin";
 import accountReducer from "./account/account";
 import clientReducer from "./client/client";
+import problemsReducer from "./problems/problems";
 
 export const store = configureStore({
     reducer:{
         admin:adminReducer,
         account:accountReducer,
-        client:clientReducer
-    }
+        client:clientReducer,
+        problems:problemsReducer,
+    },
+    middleware: [
+        ...getDefaultMiddleware({
+            serializableCheck: false
+        }),
+    ],
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
