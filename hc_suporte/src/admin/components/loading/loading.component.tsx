599adc279e90f4e3983b594052fc1c3c314150ca
wesleywil <wesleywilsonti@gmail.com>
2023-02-10 17:49:19 -0300

    refactored loading component to use less code where is imported

--- hc_suporte/src/admin/components/loading/loading.component.tsx
+++ hc_suporte/src/admin/components/loading/loading.component.tsx
@@ -2,8 +2,15 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Loading = () => {
   return (
-    <div className="text-white text-6xl animate-spin">
-      <AiOutlineLoading3Quarters />
+    <div className="h-screen">
+      <div className="h-full flex flex-col items-center justify-center">
+        <div className="self-center">
+          <div className="text-white text-6xl animate-spin">
+            <AiOutlineLoading3Quarters />
+          </div>
+        </div>
+        <h1 className="text-white">Carregando...</h1>
+      </div>
     </div>
   );
 };
