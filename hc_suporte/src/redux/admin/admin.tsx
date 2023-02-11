5b0f3020ee0d80d6ca9ff1a49d771f2e8b49e532
wesleywil <wesleywilsonti@gmail.com>
2023-02-11 16:50:42 -0300

    updated adminSlice by adding user_selected state and set_user reducer

--- hc_suporte/src/redux/admin/admin.tsx
+++ hc_suporte/src/redux/admin/admin.tsx
@@ -1,15 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { AccountState } from "../account/account";
 
 export interface AdminState {
   client_list_hidden: boolean;
   service_info_hidden: boolean;
   client_info_hidden: boolean;
+  user_selected: AccountState;
 }
 
 const initialState: AdminState = {
   client_list_hidden: true,
   service_info_hidden: true,
   client_info_hidden: true,
+  user_selected: {
+    address: "",
+    admin: "idle" || "false" || "true",
+    cellphone: "",
+    city: "",
+    displayName: "",
+    district: "",
+    email: "",
+    state: "",
+    zip_code: "",
+    uid: "idle" || "",
+  },
 };
 
 export const adminSlice = createSlice({
@@ -25,6 +39,10 @@ export const adminSlice = createSlice({
     switch_client_info_view: (state) => {
       state.client_info_hidden = !state.client_info_hidden;
     },
+    set_user: (state, { payload }) => {
+      state.client_info_hidden = !state.client_info_hidden;
+      state.user_selected = payload;
+    },
   },
 });
 
@@ -32,6 +50,7 @@ export const {
   switch_client_list_view,
   switch_service_info_view,
   switch_client_info_view,
+  set_user,
 } = adminSlice.actions;
 
 export default adminSlice.reducer;
