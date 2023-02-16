9dce30bc5b62cc110271b5d0e64999b6e4017cb5
wesleywil <wesleywilsonti@gmail.com>
2023-02-16 13:41:22 -0300

    updated withAuthenticationAdmin compnonent to show a message that the user don't have authorization to enter in the admin only pages

--- hc_suporte/src/admin/components/withAuthenticationAdmin/withAuthenticationAdmin.component.tsx
+++ hc_suporte/src/admin/components/withAuthenticationAdmin/withAuthenticationAdmin.component.tsx
@@ -1,8 +1,10 @@
 import { useEffect, ComponentType } from "react";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../../redux/store";
-import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Loading from "../loading/loading.component";
+
+import { AiFillWarning } from "react-icons/ai";
 
 function WithAuthenticationAdmin<T>(
   Component: ComponentType<T>,
@@ -19,7 +21,25 @@ function WithAuthenticationAdmin<T>(
     if (user_id !== "" && admin) {
       return <Component {...hocProps} />;
     } else {
-      return <Navigate to="/logar" replace={true} />;
+      return (
+        <div className="h-screen ">
+          <div className="h-full flex flex-col items-center justify-center">
+            <div className="text-[#d9b55d] text-6xl animate-pulse">
+              <AiFillWarning />
+            </div>
+            <div className="text-white flex flex-col gap-2  text-center text-2xl">
+              <h1>Você não tem permissão para estar aqui!</h1>
+              <h2 className="text-xl">Volte imediatamente</h2>
+              <Link
+                to="/"
+                className="w-1/2 mx-auto font-bold bg-[#d9b55d] text-[#0a191e] px-2 py-1 rounded-xl"
+              >
+                Homepage
+              </Link>
+            </div>
+          </div>
+        </div>
+      );
     }
   }
 }
